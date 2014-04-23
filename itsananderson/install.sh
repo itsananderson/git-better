743e70aaac9ac7abbdc467f980d4dc8b9098062d
Will Anderson <will@itsananderson.com>
2014-04-23 15:06:59 -0700

    Fix inclusion of main install script from user scripts

--- itsananderson/install.sh
+++ itsananderson/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-../install.sh
+rel_script_dir=`dirname $0`
+pushd $rel_script_dir > /dev/null
+script_dir=`pwd -P`
+popd > /dev/null
+
+$script_dir/../install.sh
 
 # User specific install steps
