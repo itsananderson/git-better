06fb5e62ca496a6e8e30ab4c0090d1a3b49a38f2
David Ruttka <david@davidruttka.com>
2014-04-25 22:22:10 -0700

    Fix typo for mergetool keepTemporaries
    The line to set keepTemporaries on the mergetool said difftool, setting the difftool a second time.

--- kdiff.sh
+++ kdiff.sh
@@ -18,7 +18,7 @@ git config --global merge.tool kdiff3
 git config --global mergetool.kdiff3.path "$kdiffpath"
 git config --global mergetool.kdiff3.keepBackup false
 git config --global mergetool.kdiff3.trustExitCode true
-git config --global difftool.kdiff3.keepTemporaries false
+git config --global mergetool.kdiff3.keepTemporaries false
 
 git config --global --replace-all alias.dt 'difftool -y'
 git config --global --replace-all alias.mt 'mergetool -y'
