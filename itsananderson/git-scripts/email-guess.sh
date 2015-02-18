ab7f85ed3dc09825e39dc7792a591851f47d3143
Will Anderson <will@itsananderson.com>
2015-02-18 14:37:43 -0800

    Update email-guess script with settings for Microsoft on GitHub

--- itsananderson/git-scripts/email-guess.sh
+++ itsananderson/git-scripts/email-guess.sh
@@ -3,6 +3,9 @@
 remote=`git remote -v | awk '/\(push\)$/ {print $2}'`
 email=will@itsananderson.com # default
 
+if [[ $remote == *github.com:Microsoft* ]]; then
+  email=wiand@microsoft.com
+fi
 if [[ $remote == *enr.visualstudio.com* ]]; then
   email=wiand@microsoft.com
 fi
